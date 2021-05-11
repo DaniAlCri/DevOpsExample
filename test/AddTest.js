4bd3fd75d6655c78d0da1bedabfa439bc2e2550f
Chiribito <75863800+Chiribito@users.noreply.github.com>
2021-05-11 18:04:51 +0200

    *Change to mocha unit testing
    Falla porque no estoy llamando al script (Add.js).

--- test/AddTest.js
+++ test/AddTest.js
@@ -1,16 +1,20 @@
+//F1  Mocha run all tests
 const assert = require('assert');
+//var test = require('unit.js');
+//var test = require('Add.js');
 
-var test = require('./Add.js');
 var result = 5;
+
+//https://howtodoinjava.com/javascript/jasmine-unit-testing-tutorial/
 
 describe('Testing AddScript.js', function(){
 
     it('Normal case', function(){
-      assert.equal(test.value(add(2,3), 5));
+      assert.StrictEqual(Add(2,3), 5);
       //result == test.value(add(2,3)).isEqual(5);
     });
 
-    it('Negative numbers case', function(){
+    /*it('Negative numbers case', function(){
       assert.equal(test.value(add(-7,3), -4));
       //result == test.value(add(-7,3)).isEqual(-4);
     });
@@ -23,5 +27,5 @@ describe('Testing AddScript.js', function(){
     it('No numbers case', function(){
       assert.equal(typeof test.value("a","b"), "String")
       //result == test.value(add("a","b")).isEqual(1001000000);
-    });
+    });*/
   });
