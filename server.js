ca790d59a6f2e93b2933a00ac5da1230305dc8a9
Chiribito <debugthissheet@gmail.com>
2021-06-23 18:22:59 +0200

    Dockerfile and server.js simplification
    Se han simplificado por claridad y eficiencia.

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-//Source: https://freshman.tech/learn-node/
+var http = require('http');
+var url = require('url');
 
-const express = require("express");
-const app = express();
-
-const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-  });
-
-  app.get('/home/dalvac01/Addwebsite', (req, res) => {
-    res.sendFile(__dirname + "/AddWebpage.html");
-  });
-  app.listen(3000, function () {
-      console.log("Server is running on localhost3000");
-  });
-  
+var server = http.createServer(function(req, res) {
+  res.sendFile(AddWebpage.html);
+  res.end();
+});
+server.listen(8080);
 
 
