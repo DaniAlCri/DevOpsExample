023c635f93e30f8386bd3c731f510640eef93973
Chiribito <correo.de.desarrollador.ficticio@gmail.com>
2021-09-25 16:54:22 +0200

    Testing jenkins
    Se prueba un fallo real en el archivo addscript.js para analizar el comportamiento de Jenkins.

--- www/js/AddScript.js
+++ www/js/AddScript.js
@@ -5,15 +5,16 @@ function add(text) {
 	numbers = text.split(" ");
 
 	//Correct code. This is the valid solution
+	/*
 	for (var x of numbers)
 		if(Number.isInteger(Number.parseInt(x)))
 			result += Number.parseInt(x);
-	
+	*/
 	//Wrong code. This is expected to fail unit tests.
-	/* 
+	 
 	for (var x of numbers)
 		result += Number.parseInt(x);
-	*/
+	
 
 	console.log("Result = " + result);
 	return result;
